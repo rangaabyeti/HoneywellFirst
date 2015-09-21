9c4f79a6d6f7869f64770a9989377e917296a7a4
rangaabyeti <rayapati.rangarao@gmail.com>
2015-09-21 21:47:50 +0530

    Honeywell First Coded UI Automation

---
+++ xCodedUIFramework-master/xCodedUI.AppControls/WebControls/xHtmlCustom.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+
+namespace xCodedUI.AppControls.WebControls
+{
+    /// <summary>
+    /// Inherits from Microsoft.VisualStudio.TestTools.UITesting.HtmlControls.HtmlCustom
+    /// Implements base methods and properties defined in the IxControl interface
+    /// </summary>
+    public class xHtmlCustom : HtmlCustom, IxControl
+    {
+        public xHtmlCustom(UITestControl parent) : base(parent) { }
+
+        public xHtmlCustom(UITestControl parent, string id, string searchProperty = "Id")
+            : base(parent)
+        {
+            this.SearchProperties.Add(searchProperty, id);
+        }
+
+        public void Focus()
+        {
+            this.WaitForControlReady();
+            this.SetFocus();
+        }
+
+        public void Click()
+        {
+            this.WaitForControlReady();
+            this.SetFocus();
+            Mouse.Click(this);
+        }
+
+        public void SendKeys(string text)
+        {
+            this.WaitForControlReady();
+            this.SetFocus();
+            Keyboard.SendKeys(this, text);
+        }
+
+        public void Tab()
+        {
+            this.WaitForControlReady();
+            Keyboard.SendKeys("{ENTER}");
+        }
+    }
+}
---
+++ xCodedUIFramework-master/xCodedUI.AppFramework/Pages/WorkOrderPage.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xCodedUI.AppControls;
+using xCodedUI.AppControls.WebControls;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using xCodedUI.AppControls.WinControls;
+
+namespace xCodedUI.AppFramework.Pages
+{
+    /// <summary>
+    /// Represent's the workoder page of the Honeywell First Application
+    /// </summary>
+    public class WorkOrderPage
+    {
+        private BrowserWindow _browser;
+        private xHtmlHyperlink _workorderTab;
+        private xHtmlImage _newWorkorder;
+        private xHtmlEdit _name;
+        private xHtmlDiv _select;
+        private xHtmlHyperlink _HomeTab;
+        private xHtmlInputButton _createbtn;
+        private xHtmlImage _backbtn;
+        private xWinPane _SelectService;
+        private xHtmlEdit _serialNo;
+        private xHtmlImage _serchIcon;
+        private xHtmlInputButton _Addbtn;
+        
+       
+       
+        public WorkOrderPage(BrowserWindow b)
+        {
+            b.CloseOnPlaybackCleanup = false;
+            _browser = b;
+            _workorderTab = new xHtmlHyperlink(b, "Work Orders", "InnerText");
+            _newWorkorder = new xHtmlImage(b, "btnSubmitNewWO");
+            _name = new xHtmlEdit(b, "WorkOrderName");
+            _select = new xHtmlDiv(b, "Pane", "ControlType");
+            _HomeTab = new xHtmlHyperlink(b, "Home", "InnerText");
+            _createbtn = new xHtmlInputButton(b, "btnCreate");
+            _backbtn = new xHtmlImage(b, "btnBack");
+            _SelectService = new xWinPane(b, "Pane", "k-icon k-i-arrow-s", "ControlType", "Class");
+            _serialNo = new xHtmlEdit(b, "SerialNo");
+            _serchIcon = new xHtmlImage(b, "btnGo");
+            _Addbtn = new xHtmlInputButton(b, "btnAdd");
+            
+
+        }
+
+        /// <summary>
+        /// This method is a work order to Honeywell First
+        /// </summary>
+        public string[] workorder(string name)
+        {
+            string[] a = new string[10];
+            //_workorderTab.Click();
+            //a[0]=_newWorkorder.GetProperty("FriendlyName").ToString();
+            //_newWorkorder.Click();
+            a[0] = nav_createWorkorder();
+            _name.SendKeys(name);
+            a[1] = _name.GetProperty("Text").ToString();
+            return a;   
+        }
+
+        public string nav_createWorkorder()
+        {
+            string a;
+            _workorderTab.Click();
+            a =_newWorkorder.GetProperty("Alt").ToString();
+            _newWorkorder.Click();
+            return a;
+        }
+
+        public void nav_Home()
+        {
+            _HomeTab.Click();
+        }
+        public void Click_create()
+        {
+            _createbtn.Click();
+        }
+
+        public string Click_backbtn()
+        {
+            string a;
+            _backbtn.Click();
+            a = _newWorkorder.GetProperty("FriendlyName").ToString();
+            return a;
+
+        }
+
+        public void SelectServiceProvider(BrowserWindow window)
+        {
+            UITestControl selectServices = new UITestControl(window);
+            selectServices.TechnologyName = "Web";
+            selectServices.SearchProperties.Add("ControlType", "Pane", "Class", "k-icon k-i-arrow-s");
+            Mouse.Click(selectServices);
+
+            UITestControl ServicesItem = new UITestControl(window);
+            ServicesItem.TechnologyName = "Web";
+            ServicesItem.SearchProperties.Add("TagName", "LI", "InnerText", "Minerva");
+            Mouse.Click(ServicesItem);
+
+        }
+
+        public void AddItem(string serialNo)
+        {
+            _serialNo.SendKeys(serialNo);
+            _serchIcon.Click();
+            _Addbtn.Click();  
+        }
+
+        public string SearchItem(string serialNo, BrowserWindow window)
+        {
+            string error;
+            _serialNo.SetProperty("Text", serialNo);
+            _serchIcon.Click();
+
+            UITestControl errorMessage = new UITestControl(window);
+            errorMessage.TechnologyName = "Web";
+            errorMessage.SearchProperties.Add("ControlType", "Pane", "Id", "SerialNo_validationMessage");
+            error = errorMessage.GetProperty("InnerText").ToString();
+            return error;
+
+        }
+
+    }
+}
